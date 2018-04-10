34cfd822ab44db0fb1244329ae64dfc5a88f1ffc
jbravo <jbravo@colombiahosting.com.co>
2018-04-10 12:24:34 -0500

    added git hashes to debug page

--- src/components/pages/debugPage.js
+++ src/components/pages/debugPage.js
@@ -14,7 +14,8 @@ class Debugpage extends Component {
     this.state = {
       "isLoaded":false,
       "boards": [],
-      "redirectToLogin":false
+      "redirectToLogin":false,
+      "hashes": []
     };
 
   }
@@ -38,7 +39,7 @@ class Debugpage extends Component {
         axios.post(window.customVars.urlPrefix+window.customVars.apiDebug,postData,axiosConfig)
         .then(res => {
           if (res.data.success&&res.data.boards&&res.data.boards.length>0) {
-            page.setState({"isLoaded":true,"boards":res.data.boards});
+            page.setState({"isLoaded":true,"boards":res.data.boards,"hashes":res.data.hashes});
 
           } else {
             if ((typeof res.data.token !== 'undefined')&&res.data.token!==null&&res.data.token==="expired") {
@@ -56,7 +57,7 @@ class Debugpage extends Component {
   }
 
   render() {
-    const { isLoaded, redirectToIndex,redirectToLogin,boards } = this.state;
+    const { isLoaded, redirectToIndex,redirectToLogin,boards,hashes } = this.state;
 
     if (redirectToLogin) {
       return <Redirect to="/login?expired" />;
@@ -65,7 +66,7 @@ class Debugpage extends Component {
     return (
       <div className="Debugpage">
 
-      <h1>Debug<br/><small>Miner Stats</small></h1>
+      <h1>Debug<br/><small>Miner Stats and Versions</small></h1>
 
       <div className="row">
 
@@ -73,12 +74,39 @@ class Debugpage extends Component {
            <div className="col-md-12 mt-5">
              <div className="box">
                <div className="box-header">
-                 <h3>CgMiner Stats {!isLoaded && <div className="lds-dual-ring pull-right"></div>}</h3>
+                 <h3>Miner Debug {!isLoaded && <div className="lds-dual-ring pull-right"></div>}</h3>
                </div>
                 <div className="box-body p-4">
+                <h4>GIT hashes</h4>
+                <div className="row">
+                  <div className="col-md-3 field-title">
+                    Firmware
+                  </div>
+                  <div className="col-md-9 field-value">
+                    {hashes.firmware && hashes.firmware}
+                  </div>
+                </div>
+                <div className="row mt-2">
+                  <div className="col-md-3 field-title">
+                    CgMiner
+                  </div>
+                  <div className="col-md-9 field-value">
+                    {hashes.cgminer && hashes.cgminer}
+                  </div>
+                </div>
+                <div className="row  mt-2">
+                  <div className="col-md-3 field-title">
+                    Backend
+                  </div>
+                  <div className="col-md-9 field-value">
+                    {hashes.backend && hashes.backend}
+                  </div>
+                </div>
+                <hr />
+                <h4>Boards</h4>
                 {boards.map((item, index) => (
                   <div key={index} className="mt-3">
-                    <h4>Board {index+1}</h4>
+                    <h5>Board {index+1}</h5>
 
                     <div className="row small">
                         {Object.keys(item.board).map(function(key) {
@@ -131,8 +159,9 @@ class Debugpage extends Component {
                         </table>
 
                     </div>
-
+                      <hr/>
                   </div>
+
                 ))}
                 </div>
              </div>
